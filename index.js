3beba2ffc344bd27f7830f39d62b58def6765098
e-r-i-k-a <samuels.erika@yahoo.com>
2017-09-29 13:38:10 -0400

    added carousel transitions

--- index.js
+++ index.js
@@ -4,4 +4,17 @@ window.addEventListener('scroll', function(e) {
     } else{
         document.getElementsByTagName('header')[0].style.transform = 'translateY(-4.5em)';
     }
-})
+});
+
+var carouselArray = document.getElementsByClassName('carousel-image');
+var index = 0;
+var intervalID = setInterval(function(){
+    rotateCarousel(carouselArray);
+},2500);
+
+function rotateCarousel (pix) {
+    let length = pix.length;
+    pix[index%length].classList.add('hidden');         
+    pix[(index+1)%length].classList.remove('hidden');  
+    index += 1;
+}
